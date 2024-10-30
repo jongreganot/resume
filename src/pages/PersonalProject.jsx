9b124404031f3a4d742ad3151429bf8584c5a8a6
Joshua Reganot <joshuaeli.reganot@merkle.com>
2024-10-30 10:33:00 +0800

    Added basketball

--- src/pages/PersonalProject.jsx
+++ src/pages/PersonalProject.jsx
@@ -19,13 +19,18 @@ class PersonalProject extends React.Component {
                             title="Wedding Invite"
                             description="I created this website to create something that can help me monitor and inform the guests of my wedding."
                             techStack={["AWS Serverless Architecture", "S3", "DynamoDb", "Lambda", "API Gateway", "React"]}
-                            link="https://wedding-invite.joshuasites.pro/" />
+                            link="https://joshua-and-jassy-invite.joshuasites.pro/" />
                 <ProjectItem path="M416 0C400 0 288 32 288 176V288c0 35.3 28.7 64 64 64h32V480c0 17.7 14.3 32 32 32s32-14.3 32-32V352 240 32c0-17.7-14.3-32-32-32zM64 16C64 7.8 57.9 1 49.7 .1S34.2 4.6 32.4 12.5L2.1 148.8C.7 155.1 0 161.5 0 167.9c0 45.9 35.1 83.6 80 87.7V480c0 17.7 14.3 32 32 32s32-14.3 32-32V255.6c44.9-4.1 80-41.8 80-87.7c0-6.4-.7-12.8-2.1-19.1L191.6 12.5c-1.8-8-9.3-13.3-17.4-12.4S160 7.8 160 16V150.2c0 5.4-4.4 9.8-9.8 9.8c-5.1 0-9.3-3.9-9.8-9L127.9 14.6C127.2 6.3 120.3 0 112 0s-15.2 6.3-15.9 14.6L83.7 151c-.5 5.1-4.7 9-9.8 9c-5.4 0-9.8-4.4-9.8-9.8V16zm48.3 152l-.3 0-.3 0 .3-.7 .3 .7z"
                             title="Food Order"
                             description="I created this website to practice my react fundamentals (states and routing)."
                             techStack={["React", "React Router v6", "HTML", "SCSS", "JQuery", "Javascript"]}
                             link="https://jongreganot.github.io/food-order/" />
-                            
+                <ProjectItem path="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM240 80a32 32 0 1 1 0 64 32 32 0 1 1 0-64zM208 208a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zm-64-64a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"
+                            title="Basketball"
+                            description="I created this website for our basketball league."
+                            techStack={["React", "React Router v6", "HTML", "SCSS", "JQuery", "Javascript"]}
+                            link="http://basketball-league.s3-website-ap-northeast-1.amazonaws.com/" />
+
             </div>
         )
     };
